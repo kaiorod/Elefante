141e2e9ea82505d24ae15e7bbfcfe3cc0b87c3f5
kaiorod <caiorodrigo2775@gmail.com>
2021-11-21 16:42:43 -0300

    Finaliza Elefante

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Elefante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdelefante,c;
+            string incomoda;
+            
+            Console.Beep();
+            Console.Clear();
+            inicio:
+            Console.Write("Quantos elefantes? : ");
+            qtdelefante = int.Parse(Console.ReadLine());
+
+            c = 1;
+           if(qtdelefante % 2 == 0 && qtdelefante > 2)
+           {
+            while(c <= qtdelefante)
+                {   
+                    if(c == 1)
+                    {
+                        Console.WriteLine($"{c} elefante incomoda muita gente ");   
+                        incomoda = string.Concat(Enumerable.Repeat("incomodam ", c + 1));
+                        Console.WriteLine($"{c+1} elefantes {incomoda}muito mais");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{c} elefantes incomodam muita gente "); 
+                        incomoda = string.Concat(Enumerable.Repeat("incomodam ", c + 1));
+                        Console.WriteLine($"{c+1} elefantes {incomoda}muito mais");
+                    }
+                    c = c + 2;
+                }
+           }
+           else
+           {
+               Console.ForegroundColor = ConsoleColor.DarkRed;
+               Console.WriteLine("Digite um numero par de elefantes, maior que 2");
+               Console.ResetColor();
+               goto inicio;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Elefante
-Exiba a letra da música infantil "Um Elefante Incomoda Muita Gente". Comece com 1 elefante e termine na quantidade digitada pelo usuário (um número par maior que 2).  1 elefante incomoda muita gente 2 elefantes incomodam incomodam muito mais  3 elefantes incomodam muita gente 4 elefantes incomodam incomodam incomodam incomodam muito mais Ex.:  Quantos elefantes: 6  1 elefante incomoda muita gente 2 elefantes incomodam incomodam muito mais  3 elefantes incomodam muita gente 4 elefantes incomodam incomodam incomodam incomodam muito mais  5 elefantes incomodam muita gente 6 elefantes incomodam incomodam incomodam incomodam incomodam incomodam muito mais
+Exiba a letra da música infantil "Um Elefante Incomoda Muita Gente". 
+Comece com 1 elefante e termine na quantidade digitada pelo usuário (um número par maior que 2).  
+1 elefante incomoda muita gente 
+2 elefantes incomodam incomodam muito mais  
+3 elefantes incomodam muita gente 
+4 elefantes incomodam incomodam incomodam incomodam muito mais 
+Ex.:  Quantos elefantes: 6  
+1 elefante incomoda muita gente 
+2 elefantes incomodam incomodam muito mais  
+3 elefantes incomodam muita gente 
+4 elefantes incomodam incomodam incomodam incomodam muito mais  
+5 elefantes incomodam muita gente 
+6 elefantes incomodam incomodam incomodam incomodam incomodam incomodam muito mais
